61a9cf0582b825e8d5e4454f6f0a9f58103f89b3
kamalkavin96 <kamalkavin68@gmail.com>
2025-11-22 17:25:25 +0530

    Initial commit

---
+++ src/main/java/com/kamalkavin96/pdf_generator_service/PdfGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kamalkavin96.pdf_generator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfGeneratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfGeneratorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamalkavin96/pdf_generator_service/PdfGeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamalkavin96.pdf_generator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfGeneratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
