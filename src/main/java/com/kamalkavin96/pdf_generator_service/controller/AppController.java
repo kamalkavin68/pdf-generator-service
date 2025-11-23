219a38dfd60d635264d087e0388ad644e11bc8a8
kamalkavin96 <kamalkavin68@gmail.com>
2025-11-23 08:24:00 +0530

    app controller added for adding the app config data

---
+++ src/main/java/com/kamalkavin96/pdf_generator_service/controller/AppController.java
@@ -0,0 +1,9 @@
+package com.kamalkavin96.pdf_generator_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/app")
+public class AppController {
+}
