d57172267b93ffd16a98ae310634483a75571e3f
kamalkavin96 <86216079+kamalkavin68@users.noreply.github.com>
2025-11-24 17:47:14 +0530

    Update HeaderModel.java

--- src/main/java/com/kamalkavin96/pdf_generator_service/core/model/HeaderModel.java
+++ src/main/java/com/kamalkavin96/pdf_generator_service/core/model/HeaderModel.java
@@ -3,12 +3,13 @@ package com.kamalkavin96.pdf_generator_service.core.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@EqualsAndHashCode(callSuper = true)
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
